7e387272024735dd0c5ece0da7a0c1d5246664e8
Preethihariharan11 <preethihariit03@gmail.com>
2020-03-04 19:03:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -12,8 +12,70 @@ function mul(x,y){
 function div(x,y){
   return x/y;
 }
-function add(x,y){
-  return x+y;
+function mod(x,y){
+  return x%y;
+}
+function pow(x,y){
+  return x**y;
 }
 let add=add(a,b)
-console.log(add)
+let sub=sub(a,b)
+let mul=mul(a,b)
+let div=div(a,b)
+let mod=mod(a,b)
+let pow=pow(a,b)
+console.log("addition is "+add,"subtraction is "+sub,"multiplication is "+mul,"modulus is "+mod,"power is "+pow,"division is "+div)
+//-----------------------------------------------------------------------///compare string
+let name1='preethi'
+let nam='preethi'
+function comp(y,z){
+  return (y===z)? true: false
+} 
+compare=comp(name1,nam)
+console.log(compare)
+//compare number
+one=987654
+two='987654'
+function compnum(y,z){
+  return (y===z)? true: false
+} 
+comparenumber=compnum(one,two)
+console.log(comparenumber)
+//compare type
+function type(x,y){
+  return(typeof(x)===typeof(y))
+}
+type=type(one,two)
+console.log(type)
+//-------------------------------------------------------------------
+//grade 
+function mark(x){
+  if(x<40){
+    return 'U grade';
+  }
+  if((x>40)&&(x<55)){
+    return 'E grade';
+  }
+  if((x>55)&&(x<70)){
+    return 'E grade';
+  }
+  if((x>70)&&(x<80)){
+    return 'C grade';
+  }
+  if((x>80)&&(x<90)){
+    return 'B grade';
+  }
+  else{
+    return 'A grade';
+  }
+}
+grade=mark(99)
+console.log(grade)
+
+//--------------------------------------------------------------------
+let text =['a','e','i','0','u']
+let lettr=''
+let i;
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
