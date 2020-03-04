1c26365430746dfaf6a3d1095e0d47394c5a07ab
Preethihariharan11 <preethihariit03@gmail.com>
2020-03-04 18:17:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-3cpptg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-3cpptg)
---
+++ index.js
@@ -0,0 +1,19 @@
+let a=2089;
+let b=11;
+function add(x,y){
+  return x+y;
+}
+function sub(x,y){
+  return x-y;
+}
+function mul(x,y){
+  return x*y;
+}
+function div(x,y){
+  return x/y;
+}
+function add(x,y){
+  return x+y;
+}
+let add=add(a,b)
+console.log(add)
